7374c96cbae0ff12d4e6bb74f06a0d8c23356892
superboxes <red.reduose@gmail.com>
2022-07-25 20:16:22 +0300

    added help command filtering by categories, also please for the love of god just fix the errors in all of the other files

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -14,7 +14,8 @@ module.exports = (client) =>{
                 const command = require(`${client.commandsPath}/${category}/${file}`)
                 
                 if(command.name){
-                    client.commands.set(command.name , command)
+                  if (!command.category && client.autoHelpMenu) throw '[sdhandler]: You must have a categories for your command(s) if youre using autoHelpMenu'
+                  client.commands.set(command.name , command)
                     console.log(`${command.name} : ✅`)
                     if(command.slash){
                         client.slashcommands.set(command.name , command)
@@ -32,7 +33,6 @@ module.exports = (client) =>{
         const commandFiles = readdirSync(`${client.commandsPath}`).filter(file => file.endsWith('.js'))
             for(const file of commandFiles){
                 const command = require(`${client.commandsPath}/${file}`)
-    
                 if(command.name){
                     client.commands.set(command.name , command)
                     console.log(`${command.name} : ✅`)
--- plugins/helpCommand.js
+++ plugins/helpCommand.js
@@ -1,27 +1,47 @@
+const {Constants, Client, MessageEmbed} = require('discord.js')
+
+/**
+  * @async
+  * @param {Client} client
+  */
+
 module.exports = {
     name: 'help',
-    descriptions: 'Shows all the commands the bot has',
+    description: 'Shows all the commands the bot has',
     type: Constants.ApplicationCommandTypes.CHAT_INPUT,
     slash: 'both',
-    async execute ({interaction, messsage}) {
-        const commands = client.commands.entries()
+  category: 'server', 
+    async execute ({interaction, message, client}) {
+      const embed = new MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle('Help')
+      .setDescription('This is a list of all the commands the bot has')
 
-        for (const command of commands) {
-            const data = command[1]
+      // Get the commands
+      const commands = client.commands.entries()
+      let categories = []
+      let finishedCommands = []
+      for (const command of commands) {
+        data = command[1]
+        categories.push(data.category)
+        finishedCommands.push(data)
+      }
 
-            embed.addField(data.name, data.description)
+      let uniqueCategories = [...new Set(categories)]
+      // Find the commands in each category
+      for (const category of uniqueCategories) {
+        let commandsInCategory = finishedCommands.filter(command => command.category === category)
+        let categoryString = ''
+        for (const command of commandsInCategory) {
+          categoryString += `${command.name} - ${command.description}\n`
         }
+        embed.addField(category, categoryString)
+      }
+      if (interaction) {
+        interaction.reply({embeds: [embed]})
+      } else if (message) {
+        message.reply({embeds: [embed]})
+      }
 
-        const embed = new MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle("Help")
-        .setDescription('Here are all the commands you can use!')
-        if (interaction) {
-            embed.setFooter(interaction.client.user.tag)
-            interaction.reply({ embeds: [embed], ephemeral: true, })
-        } else if (message) {
-            embed.setFooter(messsage.client.user.tag)
-            message.reply( { embeds: [embed], ephemeral: true, })
-        }
     }
 }
